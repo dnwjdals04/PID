2cdc22b76efb7370ba5fde6d212987852d4b814a
J-Min <dnwjdals04@korea.ac.kr>
2025-11-05 16:18:43 +0900

    Delete frontend directory

--- frontend/components/LoadingSkeleton.js
+++
@@ -1,8 +0,0 @@
-export default function LoadingSkeleton() {
-  return (
-    <div className="loading-skeleton">
-      <div className="skeleton-box" />
-      <p>AI 분석 중입니다...</p>
-    </div>
-  );
-}
--- frontend/lib/api.js
+++
@@ -1,8 +0,0 @@
-// frontend/lib/api.js
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
-});
-
-export default instance;
